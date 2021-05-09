f0e6796aafa10360e4474fc280af219ed2c63b1e
Amit Singh Dalal <amit.singh@magehost.com>
2021-05-09 18:57:19 +0530

    updated def

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Amit Dalal"));
+app.get("/", (req, res) => res.send("Hello Amit Dalal from vscode"));
 
 app.get("/health", (req, res) => {
     res.status(200);
