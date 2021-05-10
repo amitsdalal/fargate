a02799f1d610ddfd13eab44a060a0b849559f555
Amit Dalal <sameerashle@gmail.com>
2021-05-10 22:27:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Amit Dalal from vscode"));
+app.get("/", (req, res) => res.send("Hello Amit Dalal from GitHub"));
 
 app.get("/health", (req, res) => {
     res.status(200);
